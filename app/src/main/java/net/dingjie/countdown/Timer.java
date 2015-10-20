fa1cf352e60a474f558ddb258d3c0c6e14287424
windssmile <cinasthana@gmail.com>
2015-10-20 20:30:24 +0800

    fixed a bug

--- app/src/main/java/net/dingjie/countdown/Timer.java
+++ app/src/main/java/net/dingjie/countdown/Timer.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,16 +35,20 @@ public class Timer extends AppCompatActivity {
             }
         }
     };
-    View.OnClickListener vocl = new View.OnClickListener() {
+    OnClickListener ocl = new OnClickListener() {
         @Override
         public void onClick(View v) {
             if (remain == 0) {
                 remain = myTimer.getRestTime();
+                Log.d("OnClickListener", myTimer.toString());
                 myTimer.cancel();
+                Log.d("OnClickListener", myTimer.toString());
                 Toast.makeText(Timer.this, R.string.pauseString, Toast.LENGTH_SHORT).show();
                 buttonStop.setText(R.string.resumeString);
             } else {
+                Log.d("OnClickListenerBefore", myTimer.toString());
                 myTimer = new MyTimer(remain);
+                Log.d("OnClickListenerAfter", myTimer.toString());
                 myTimer.start();
                 remain = 0;
                 Toast.makeText(Timer.this, R.string.resumeString, Toast.LENGTH_SHORT).show();
@@ -49,26 +56,36 @@ public class Timer extends AppCompatActivity {
             }
         }
     };
+    private AlertDialog isExit;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (buttonStop.isClickable()) {
+            isExit.show();
+        } else {
+            myTimer.cancel();
+            finish();
+        }
+        return true;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-        getData();
-        inter(myTimer, countDown);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        prepare();
+        inter();
 
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && buttonStop.isClickable()) {
-            AlertDialog isExit = new AlertDialog.Builder(this).create();
-            isExit.setTitle(R.string.app_name);
-            isExit.setMessage(getString(R.string.exit));
-            isExit.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.yes), exitListener);
-            isExit.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.no), exitListener);
             isExit.show();
         } else {
+            myTimer.cancel();
             finish();
         }
         return false;
@@ -107,18 +124,23 @@ public class Timer extends AppCompatActivity {
 
     }
 
-    private void getData() {
+    private void prepare() {
         Intent intent = getIntent();
         Bundle bundle = intent.getBundleExtra("bundle");
         countDown = ((long) bundle.getInt("hour") * 60 * 60 * 1000) + ((long) bundle.getInt("minute") * 60 * 1000) + ((long) bundle.getInt("second") * 1000);
         display = (TextView) findViewById(R.id.textDisplay);
+        isExit = new AlertDialog.Builder(this).create();
+        isExit.setTitle(R.string.app_name);
+        isExit.setMessage(getString(R.string.exit));
+        isExit.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.yes), exitListener);
+        isExit.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.no), exitListener);
     }
 
-    private void inter(MyTimer myTimer, long countDown) {
+    private void inter() {
         myTimer = new MyTimer(countDown);
-        myTimer.start();
         buttonStop = (Button) findViewById(R.id.Button_stop);
-        buttonStop.setOnClickListener(vocl);
+        buttonStop.setOnClickListener(ocl);
+        myTimer.start();
     }
 
     protected class MyTimer extends CountDownTimer{
